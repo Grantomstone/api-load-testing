0258d171a6781e26cac2b53820f6f42d4ac75647
Aditya Sunil Kulkarni <adityakulkarni61@gmail.com>
2026-06-22 16:24:13 +0530

    basic load balanced setup

---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.14-alpine
+
+WORKDIR /src
+COPY requirements.txt ./
+COPY main.py .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["fastapi", "run"]
+
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+import random
+import time
+import os
+
+instance = os.getenv("INSTANCE_ID", default="Instance_Unknown")
+app = FastAPI()
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app)
+instrumentator.expose(app)
+
+@app.get("/")
+async def getRoot():
+	return {"status":"ok", "instance":instance}
+
+@app.get("/api/compute")
+async def compute():
+	work_ms = random.randint(30,500)
+	time.sleep(work_ms /1000)
+	return {"result": f"computed in {work_ms}ms", "instance":instance}
+
+@app.get("/health")
+async def health():
+	return {"healthy": True}
